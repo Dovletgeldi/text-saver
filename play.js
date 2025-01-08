b8c3b98c44459e9bc3b873018c39022381686e0d
Dovletgeldi Myradov <dovletm7@gmail.com>
2025-01-08 22:46:43 +0300

    Text Saver with basics

---
+++ play.js
@@ -0,0 +1,45 @@
+const saveBtn = document.getElementById("sv-msg-input");
+const titleText = document.getElementById("msg-input-title")
+const contentText = document.getElementById("msg-input")
+const savedList = document.getElementById("saved-list")
+
+saveBtn.addEventListener("click", () => {
+  const title = titleText.value.trim()
+  const paragraph = contentText.value.trim()
+  
+  if(!title || !paragraph) {
+    alert("Both title and paragraph are required!")
+    return
+  }
+  
+  const savedData = JSON.parse(localStorage.getItem("savedItems")) || []
+  savedData.push({title, paragraph})
+  localStorage.setItem("savedItems", JSON.stringify(savedData))
+
+  createListItem(title, paragraph)
+
+  titleText.value = ""
+  contentText.value = ""
+})
+
+function createListItem(title, paragraph) {
+  const item = document.createElement("div")
+  item.classList.add("saved-item")
+  item.textContent = title
+  item.setAttribute("data-paragraph", paragraph)
+  
+  item.addEventListener("click", () => {
+    navigator.clipboard.writeText(paragraph).then(() => {
+      alert(`Paragraph copied: ${paragraph}`)
+    }).catch(err => {
+      console.error("Failed to copy", err)
+    })
+  })
+
+  savedList.appendChild(item)
+
+}
+
+
+
+
